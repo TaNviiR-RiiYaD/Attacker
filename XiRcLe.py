12570e9a23c9ac928a2bdfcd6bdc9e613a74cc0a
TaNviiR-RiiYaD <80155708+TaNviiR-RiiYaD@users.noreply.github.com>
2021-05-07 17:42:26 +0600

    Update XiRcLe.py

--- XiRcLe.py
+++ XiRcLe.py
@@ -119,13 +119,11 @@ while tt<2:
 		print(green+"\n\t\t[✓] Total Sent : "+yellow+str(totalsent))
 		print(red+"\n\t\t[×] Total Not Sent : "+yellow+str(totalnotsent)+"\n")
 		lastt=str(input(purple+"\n\n\t\t  [✓] All Done!\n\t [•] Now Press Enter Key To Continue:\n"))
-		os.system('clear')
-              else:
+		count=1
+	else:
 		clear()
 		notice=red+"\t\t[×] Wrong Option Entered!"
 		count=1
-		header()
-		opt()
 
 
 
